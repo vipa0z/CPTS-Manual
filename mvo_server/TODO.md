8d4ef88ea8133b2e379fbd82d3a1c36866fef918
vipa0z <91535529+vipa0z@users.noreply.github.com>
2025-12-08 15:52:00 +0200

    x

--- mvo_server/TODO.md
+++
@@ -1,8 +0,0 @@
-# TODO
-
-- [ ] Add item generation script for the lootbox system
-- [ ] Write  tests for Achievement service, config service, and memmory loading
-- [ ] refactor and clean achievements, config, and loader code
-- [ ] add type safety using Zod or by rewriting in TypeScript
-- [ ] Write the GUI using a frontend Framework
-- [ ] Revise the achievements generation script
--- mvo_server/data/categoryMappings.js
+++
@@ -1,9 +0,0 @@
-
-const CATEGORY_CONFIGS = {
-    "shop_items_data": { filename: "shop_items_data.json", key: "shop_items_data" },
-    "wheel_items_data": { filename: "wheel_items_data.json", key: "wheel_items_data" },
-    "playtime_draw_data": { filename: "playtime_draw_data.json", key: "playtime_draw_data" },
-    "achievements_data": { filename: "achievements_data.json", key: "achievements" },
-};
-
-module.exports = { CATEGORY_CONFIGS };
